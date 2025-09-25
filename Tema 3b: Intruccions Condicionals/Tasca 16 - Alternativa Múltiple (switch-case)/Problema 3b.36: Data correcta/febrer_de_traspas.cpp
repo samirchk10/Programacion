05cd0e97f4a4545b564d8754d188c43a32ace3ee
Samir Channagui <1795488@uab.cat>
2025-09-25 12:09:44 +0200

    Segunda parte Data_Correcta

--- Tema 3b: Intruccions Condicionals/Tasca 16 - Alternativa Múltiple (switch-case)/Problema 3b.36: Data correcta/febrer_de_traspas.cpp
+++ Tema 3b: Intruccions Condicionals/Tasca 16 - Alternativa Múltiple (switch-case)/Problema 3b.36: Data correcta/febrer_de_traspas.cpp
@@ -2,3 +2,16 @@
 
 using namespace std;
 
+int main() {
+    int any;
+
+    cout << "Introdueix un any: ";
+    cin >> any;
+
+    if (any % 4 == 0 && any % 100 != 0 || any % 400 == 0)
+        cout << "Febrer té 29 dies" << endl;
+    else
+        cout << "Febrer té 28 dies" << endl;
+    
+    return 0;
+}
